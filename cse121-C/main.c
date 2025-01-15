6d35ad31db7c1da729f93687d676454737d9e047
Carson Smith <[carsonpenn@byui.edu]>
2025-01-15 15:40:43 -0700

    Initial Commit

---
+++ cse121-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  cse121-C
+//
+//  Created by Carson Smith on 1/15/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
